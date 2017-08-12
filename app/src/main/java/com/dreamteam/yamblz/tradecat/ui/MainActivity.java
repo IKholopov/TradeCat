d8b414a1a5d1115507fa800723494d347028d6a4
Maksim Sukhotski <suhockii@gmail.com>
2017-08-12 14:08:21 +0300

    Added profile GraphList fragment

--- app/src/main/java/com/dreamteam/yamblz/tradecat/ui/MainActivity.java
+++ app/src/main/java/com/dreamteam/yamblz/tradecat/ui/MainActivity.java
@@ -1,16 +1,31 @@
 package com.dreamteam.yamblz.tradecat.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dreamteam.yamblz.tradecat.ui.GraphListFragment;
 
 import com.dreamteam.yamblz.tradecat.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int container = R.id.container;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportFragmentManager().findFragmentByTag(GraphListFragment.TAG) == null) {
+            navigateToGraphList();
+        }
+    }
+
+    public void navigateToGraphList() {
+        GraphListFragment fragment = new GraphListFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(container, fragment, GraphListFragment.TAG)
+                .addToBackStack(null)
+                .commitAllowingStateLoss();
     }
 
 
