5160a4994d2ff3770cbf0698ea364c98ee67a800
gchubarich <gchubarich@gmail.com>
2017-08-12 14:27:56 +0300

    Add graphs fragment

--- app/src/main/java/com/dreamteam/yamblz/tradecat/MainActivity.java
+++ app/src/main/java/com/dreamteam/yamblz/tradecat/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dreamteam.yamblz.tradecat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dreamteam.yamblz.tradecat.ui.graphlist.GraphListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new GraphListFragment())
+                .commit();
     }
 }
